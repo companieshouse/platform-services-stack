e09b6301499294c7b491abf89863f2d86e13bf74
Lee Seviour <lseviour@companieshouse.gov.uk>
2023-10-09 13:25:21 +0100

    Update nameing

--- groups/infrastructure/locals.tf
+++ groups/infrastructure/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  stack_name     = "platform-service"
+  stack_name     = "platform-services"
   stack_fullname = "${local.stack_name}-stack"
   name_prefix    = "${local.stack_name}-${var.environment}"
 
